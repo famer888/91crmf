4de2042fa15dadae605673d146ea16cc395ecf79
nanf <>
2026-02-12 23:03:53 +0800

    fix: 回滚appName

--- lib/ui_layer/screens/crack/crack_app_type.dart
+++ lib/ui_layer/screens/crack/crack_app_type.dart
@@ -8,8 +8,8 @@ enum CrackAppType {
   pzhan(type: 29, appName: 'pzhan', topNavApi: 'navigationpzhan/index'),
   gd(type: 30, appName: 'gd', topNavApi: 'elementgd/getElementById'),
   xiaolan(type: 31, appName: 'xiaolan', topNavApi: 'tabnewxiaolan/index'),
-  hjsq(type: 32, appName: 'haijiao', topNavApi: 'elementhjsq/getElementById'),
-  tiktok51(type: 33, appName: '51tiktok', topNavApi: 'navigation51tikok/index'),
+  hjsq(type: 32, appName: 'hjsq', topNavApi: 'elementhjsq/getElementById'),
+  tiktok51(type: 33, appName: '51tikok', topNavApi: 'navigation51tikok/index'),
   ;
 
   final int type;
